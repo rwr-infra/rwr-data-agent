8283e8ebd42199d89118c29aa5b3777016e4126e
Kreedzt <zhaozisong1@live.com>
2026-05-07 14:32:13 +0800

    :wrench: lazy init

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,20 @@
 import { buildApp } from '../dist/app.js';
 
-const app = await buildApp();
+let app: Awaited<ReturnType<typeof buildApp>> | null = null;
+let initPromise: Promise<Awaited<ReturnType<typeof buildApp>>> | null = null;
 
-export default app;
+async function getApp() {
+  if (app) return app;
+  if (!initPromise) initPromise = buildApp();
+  app = await initPromise;
+  return app;
+}
+
+export default async function handler(
+  req: import('http').IncomingMessage,
+  res: import('http').ServerResponse,
+) {
+  const server = await getApp();
+  await server.ready();
+  server.server.emit('request', req, res);
+}
