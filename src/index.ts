8c57dfc60536c0db7abd5091e837ccccaaf3e026
Kreedzt <zhaozisong1@live.com>
2026-05-13 16:52:02 +0800

    feat(config): conditionally load Langfuse instrumentation based on config flag

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import { config } from './config/index.js';
 import { buildApp } from './app.js';
+
+if (config.langfuseEnabled) {
+  await import('./instrumentation.js');
+}
 
 const app = await buildApp();
 
